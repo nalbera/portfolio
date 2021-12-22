fec1235f8511641a656e53c41c354fa7a7ff013c
Nelson Albera <nalbera@hotmail.com>
2021-12-22 17:39:40 -0300

    estructura terminada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,31 @@
-import { AppBar, Drawer, IconButton, List, Toolbar } from '@material-ui/core'
+import { AppBar, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, Toolbar } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import React, { useState } from 'react'
 import logo from '../images/code.png';
 import {Link, animateScroll as scroll} from 'react-scroll';
-import { BuildTwoTone, ContactMailTwoTone, EmojiObjectsTwoTone, InfoTwoTone} from '@material-ui/icons';
+import { BuildTwoTone, Cancel, ContactMailTwoTone, EmojiObjectsTwoTone, InfoTwoTone} from '@material-ui/icons';
 import MenuIcon from '@material-ui/icons/Menu'
-
 
 const links = [
         {
                 id:"about",
                 text:"Sobre Mi",
-                icon: <InfoTwoTone/>
+                icon: <InfoTwoTone fontSize='large'/>
         },
         {
                 id:"skills",
                 text:"Technical Skills",
-                icon: <EmojiObjectsTwoTone/>
+                icon: <EmojiObjectsTwoTone fontSize='large'/>
         },
         {
                 id:"mywork",
                 text:"Mis Proyectos",
-                icon: <BuildTwoTone/>
+                icon: <BuildTwoTone fontSize='large'/>
         },
         {
                 id:"contact",
                 text:"Contacto",
-                icon: <ContactMailTwoTone/>
+                icon: <ContactMailTwoTone fontSize='large'/>
         },
         
 ];
@@ -34,7 +33,7 @@ const links = [
 
 const Navbar = () => {
         const classes = useStyles();
-        const [open, setOpen] = useState(false);
+        const [open, setOpen] = useState(true);
         return (
                 <>
                 <AppBar position='sticky' className={classes.root}>
@@ -47,15 +46,30 @@ const Navbar = () => {
                                             ))
                                     }
                             </List>
-                            <IconButton className={classes.menubutton} edge="end">
+                            <IconButton className={classes.menubutton} onClick={() => setOpen(true)} edge="end">
                                 <MenuIcon />
                           </IconButton>
                     </Toolbar>
                 </AppBar>
                 <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
-                        <IconButton> {/*hace clicable un icono*/}
-
+                        <IconButton  onClick={() => setOpen(false)}> {/*hace clicable un icono*/}
+                                    <Cancel fontSize='large' className={classes.cancelIcon}/>
                         </IconButton>
+                        <Divider/>
+                        {
+                           links.map(({id,text, icon},index) => (
+                                <Link className={classes.sidebar} key={index} to={id} spy={true} smooth={true} duration={500} offset={-70} activeClass='active'>
+                                        <ListItem component="h5">
+                                                <span>
+                                                        <ListItemIcon>
+                                                                {icon}
+                                                        </ListItemIcon>
+                                                </span>
+                                                {text}
+                                        </ListItem>
+                                </Link>
+                          ))
+                        }
 
                 </Drawer>
                 </>
@@ -110,6 +124,29 @@ const useStyles = makeStyles((theme) => ({
                         position:"absolute",
                         right:10,
                 } 
+        },
+        
+        cancelIcon:{
+                color:"tomato",
+                
+        },
+        
+        sidebar:{
+                width:"40vw",
+                [theme.breakpoints.down("sm")]:{
+                        width:"60vw",
+                },
+                "& h5":{
+                        margin: theme.spacing(10,0,0,4),
+                        fontSize:"1,4rem",
+                        color:"#333",
+                        fontWeight:"bold",
+                },
+                "& h5:hover":{
+                        color:"tomato",
+                        cursor:"pointer",
+                }
+
         },
 }))
 export default Navbar
